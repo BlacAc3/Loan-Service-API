41a371d93809ff4be218e9a1ffd4217916986eae
blacac3 <aceezeala@gmail.com>
2025-03-11 16:39:35 +0100

    attempting cors fix: allowing all

--- api/settings.py
+++ api/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'api.urls'
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
 "http://zloan.vercel.app",
 "http://localhost:5173",
