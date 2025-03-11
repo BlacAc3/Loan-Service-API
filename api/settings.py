faf2d12ac04010e1f3abd52174b8b7008e9a1123
blacac3 <aceezeala@gmail.com>
2025-03-11 16:28:10 +0100

    attempting cors fix2

--- api/settings.py
+++ api/settings.py
@@ -68,7 +68,9 @@ ROOT_URLCONF = 'api.urls'
 
 CORS_ALLOWED_ORIGINS = [
 "https://zloan.vercel.app",
-"http://localhost:5173"
+"http://localhost:5173",
+"https://vercel.com/blacac3s-projects/zloan/Df2NebiHrC5A9WJJNhBsqXAQdUM6",
+"https://zloan-blacac3s-projects.vercel.app"
 ]
 
 TEMPLATES = [
